93589edc707fe86544b3acdf1e80b1c17a3251ea
Pablo Zagni <pablozagniyahoo.com.ar>
2023-07-04 22:30:46 -0300

    responsive

---
+++ resources/views/components/inputs-select2.blade.php
@@ -0,0 +1,61 @@
+@props([
+    'label',
+    'name',
+    'value',
+    'options' => [],
+])
+<div class="md:grid sm:grid-cols-1 md:grid-cols-3 mb-3">
+    <x-inputs-label>{{ $label }}</x-inputs-label>
+    <div class="w-full md:col-span-2">
+        <div
+            x-data="{
+                multiple: false,
+                value: '{{ $value }}',
+                options: [
+                    @foreach ($options as $key=>$op)
+                        { value: {{ $key }}, label: '{{ $op }}' },
+                    @endforeach
+                ],
+                init() {
+                    let bootSelect2 = () => {
+                        let selections = this.multiple ? this.value : [this.value]
+        
+                        $(this.$refs.select).select2({
+                            multiple: this.multiple,
+                            data: this.options.map(i => ({
+                                id: i.value,
+                                text: i.label,
+                                selected: selections.map(i => String(i)).includes(String(i.value)),
+                            })),
+                        })
+                    }
+        
+                    let refreshSelect2 = () => {
+                        $(this.$refs.select).select2('destroy')
+                        this.$refs.select.innerHTML = ''
+                        bootSelect2()
+                    }
+        
+                    bootSelect2()
+        
+                    $(this.$refs.select).on('change', () => {
+                        let currentSelection = $(this.$refs.select).select2('data')
+        
+                        this.value = this.multiple
+                            ? currentSelection.map(i => i.id)
+                            : currentSelection[0].id
+                    })
+        
+                    this.$watch('value', () => refreshSelect2())
+                    this.$watch('options', () => refreshSelect2())
+                },
+            }"        
+        >
+            <select 
+                x-ref="select" 
+                {{ $attributes->merge(['class'=>'w-full form-control'])}}
+                @if (isset($name)) name="{{ $name }}" @endif
+                ></select>
+        </div>
+    </div>
+</div>
