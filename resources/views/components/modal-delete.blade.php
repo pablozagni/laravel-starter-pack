9c6d1d8cd042b7f196867535454af307989781e4
Pablo Zagni <pablozagniyahoo.com.ar>
2023-07-04 22:40:01 -0300

    modal delete

--- resources/views/components/modal-delete.blade.php
+++ resources/views/components/modal-delete.blade.php
@@ -1,62 +1,29 @@
-@props([
-    'route' => '',
-    'id',
-    'label' => 'Borrar',
-    'deleteName' => ''
-    ])
-
-<div x-data="{ deleteModal{{ $id }}: false }" class="flex justify-center">
-    {{-- Button --}}
-    <span x-on:click="deleteModal{{ $id }} = true">
-        <button type="button" class="btn btn-sm btn-danger">{{ $label }}</button>
-    </span>
-
-    <!-- Modal -->
-    <div
-        x-show="deleteModal{{ $id }}"
-        style="display: none"
-        x-on:keydown.escape.prevent.stop="deleteModal{{ $id }} = false"
-        role="dialog"
-        aria-modal="true"
-        x-id="['modal-title']"
-        :aria-labelledby="$id('modal-title')"
-        class="fixed inset-0 z-10 overflow-y-auto"
-    >
-        <!-- Overlay -->
-        <div x-show="deleteModal{{ $id }}" x-transition.opacity class="fixed inset-0 bg-black bg-opacity-50"></div>
-
-        <!-- Panel -->
-        <div
-            x-show="deleteModal{{ $id }}" x-transition
-            x-on:click="deleteModal{{ $id }} = false"
-            class="relative flex min-h-screen items-center justify-center p-4"
-        >
-            <div
-                x-on:click.stop
-                x-trap.noscroll.inert="deleteModal{{ $id }}"
-                class="relative w-full max-w-2xl overflow-y-auto rounded-xl bg-white p-12 shadow-lg"
-            >
-                {{-- Boton X --}}
-                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" @click="deleteModal{{ $id }} = false">
+<div wire:ignore.self id="deleteModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+    <div class="relative w-full h-full max-w-2xl md:h-auto">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Borrar</h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="deleteModal">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">Close modal</span>
                 </button>
-
-                <!-- Title -->
-                <h2 class="text-3xl font-bold" :id="$id('modal-title')">Borrar</h2>
-
-                <!-- Content -->
-                <p class="mt-2 text-gray-600">Está seguro que desea borrar <strong>{{ $deleteName }}</strong>?</p>
-
-                <!-- Buttons -->
-                <div class="mt-8 flex space-x-2">
-                    <form action="{{ $route }}" method="POST" class="">
-                        @csrf
-                        @method('DELETE')
-                        <button x-on:click="deleteModal{{ $id }} = false" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Si, estoy seguro</button>
-                        <button x-on:click="deleteModal{{ $id }} = false" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancelar</button>
-                    </form>
-                </div>
+            </div>
+            <div class="p-6 space-y-6">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    Está seguro que desea eliminar <strong>{{ $deleteName }}</strong>?
+                </p>
+            </div>
+            <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button 
+                    wire:click="delete()"
+                    data-modal-hide="deleteModal"
+                    type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >Si, estoy seguro</button>
+                <button
+                    data-modal-hide="deleteModal"
+                    type="button"
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                    >Cancelar</button>
             </div>
         </div>
     </div>
